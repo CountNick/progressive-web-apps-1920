ae9df2f482033fcd572a6a099b7891eb859d2838
CountNick <meijer.nick1@gmail.com>
2020-03-09 13:25:32 +0100

    :construction: Setup
    Installing packages from NPM and other things i need

--- server/app.js
+++ server/app.js
@@ -1,11 +1,24 @@
+require("dotenv").config();
 const express = require('express')
+const path = require('path');
 const app = express()
-const port = 3000
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(port, function(){
-    console.log("Dev app listening on port: " + port)
+const port = process.env.PORT || 3000
+
+app
+.use(notFound)
+.use(express.static(path.join(__dirname, 'static')))
+.set('view engine', 'ejs')
+.set('views', `${__dirname}/view/pages`)
+.get('/', home)
+
+function home(req, res){
+    res.send('Home')
+}
+
+function notFound(req, res){
+    res.status(404).send('404 not found')
+}
+
+app.listen(port, () => {
+    console.log('Dev app listening on port: ' + port)
 })

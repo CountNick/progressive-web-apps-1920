bb1464692967e88240c7da78d020dd14419df9c9
CountNick <meijer.nick1@gmail.com>
2020-03-15 16:44:09 +0100

    :wrench: Webpack config
    Setiing up webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
     entry: './server/static/js/search.js',
     output:  { 
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(`${__dirname}/server/static`, 'dist')
     }
 }

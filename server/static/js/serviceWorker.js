01b0020f41bb8d5caee97bfd859c1f22906a097b
CountNick <meijer.nick1@gmail.com>
2020-03-16 21:27:08 +0100

    :bug: ServiceWorker
    Fixing cashing issue

--- server/static/js/serviceWorker.js
+++ server/static/js/serviceWorker.js
@@ -1,16 +1,31 @@
 const CACHE_NAME = 'total-rickall-cache-v1'
 const urlsToCache = [
     '/',
-    '../css/styles.css'
+    '/css/styles.css'
 ]
 
-self.addEventListener('install', function(event) {
-    // Perform install steps
+self.addEventListener('install', (event) => {
+    console.log('Service worker install event!');
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache){
-                console.log('Opened cache')
-                return cache.addAll(urlsToCache)
-            })
-    )
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+
+  self.addEventListener('activate', (event) => {
+    console.log('Servive worker activated!')
+  })
+
+  self.addEventListener('fetch', event => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(caches.match(event.request)
+      .then(cachedResponse => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+          return fetch(event.request);
+        })
+      );
   });

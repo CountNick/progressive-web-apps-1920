9ce4ecc0cf9877d407b31c55e16ad810b045a8c9
CountNick <meijer.nick1@gmail.com>
2020-03-16 14:14:32 +0100

    :bug: image reflow
    Trying to fix img reflow issue

--- server/static/js/serviceWorker.js
+++ server/static/js/serviceWorker.js
@@ -1,14 +1,16 @@
-// importScripts('/cache-polyfill.js');
+const CACHE_NAME = 'total-rickall-cache-v1'
+const urlsToCache = [
+    '/',
+    '../css/styles.css'
+]
 
-
-// self.addEventListener('install', function(e) {
-//  e.waitUntil(
-//    caches.open('airhorner').then(function(cache) {
-//      return cache.addAll([
-//        '/',
-//        '/index.html',
-//        '/css/styles.css'
-//      ]);
-//    })
-//  );
-// });
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache){
+                console.log('Opened cache')
+                return cache.addAll(urlsToCache)
+            })
+    )
+  });

357af79a07dd9613cd9870fe6d95e8b444bb925f
CountNick <meijer.nick1@gmail.com>
2020-03-20 17:51:40 +0100

    :package: Package update
    Update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 const path = require('path')
 
 module.exports = {
-    entry: 
-            './server/static/js/search.js',
-    output:  { 
-        filename: 'bundle.js',
-        path: path.resolve(`${__dirname}/server/static`, 'dist')
-    },
+    watch: true,
+    mode: 'production',
+    entry: './server/static/js/search.js',
     module: {
         rules: [
             {
@@ -15,7 +12,18 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css/,
+                use: {
+                    loader: 'css-loader'
+                }
             }
         ]
-    }
+    },
+    output:  { 
+        filename: 'bundle.js',
+        path: path.resolve(`${__dirname}/server/static`, 'dist')
+    },
+    watch: false
 }
